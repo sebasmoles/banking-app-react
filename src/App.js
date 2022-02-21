795139bb3bb827300946d231802898938a0a7a38
Sebastian <roshfeltsemo@gmail.com>
2022-02-21 11:57:51 -0500

    add(App): example transactions data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import BalancePage from './routes/BalancePage';
 import TransactionPage from './routes/TransactionPage';
 import NotFoundPage from './routes/NotFoundPage';
+import { v4 as uuidv4 } from 'uuid';
 import './scss/index.scss';
 
 function App() {
 	// Total balance amount
 	const [amountTotal, setAmountTotal] = useState(0);
-	// Transactions history
-	const [txns, setTxns] = useState([]);
+	// Transactions history with example data
+	const [txns, setTxns] = useState([
+		{
+			id: uuidv4(),
+			date: '2022-02-01',
+			description: 'BANKING APP DES:CCD+',
+			type: 'Deposit',
+			amount: 30000,
+			balance: 30000
+		},
+		{
+			id: uuidv4(),
+			date: '2022-02-07',
+			description: 'BANKING APP DES:CCD+',
+			type: 'Withdraw',
+			amount: 25000,
+			balance: 5000
+		},
+		{
+			id: uuidv4(),
+			date: '2022-02-14',
+			description: 'BANKING APP DES:CCD+',
+			type: 'Withdraw',
+			amount: 5000,
+			balance: 0
+		}
+	]);
+	// Sort example data in first load
+	useEffect(() => {
+		setTxns(txns.reverse());
+	}, []);
 	return (
 		<Router>
 			<div className="container">
